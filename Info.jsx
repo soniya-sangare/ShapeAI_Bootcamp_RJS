5a05d575f6f95635850bc43623d74ed31fbd82fe
soniya-sangare <80531210+soniya-sangare@users.noreply.github.com>
2021-06-27 20:20:10 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        A basic Web dev React js Bootcamp. This was an amazing bootcamp taken by
+        Shaurya Sinha.
+      </p>
+    </div>
+  );
+}
+export default Info;
